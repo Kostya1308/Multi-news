ec9128a38976ed0b0d6197cdaf352c6985bef9ea
Constantine Piskunov <konstantinpiskunov1308@gmail.com>
2023-04-28 11:42:19 +0300

    Fixed RestControllerExceptionHandler. Methods return ResponceEntity now.

--- news/src/main/java/by/clevertec/exception/RestControllerExceptionHandler.java
+++ news/src/main/java/by/clevertec/exception/RestControllerExceptionHandler.java
@@ -1,20 +1,14 @@
 package by.clevertec.exception;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class RestControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NewsNotFoundException.class, CommentNotFoundException.class})
-    public void handleNotFoundConflict() {
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(RuntimeException.class)
-    public void handleJsonProcessingConflict(){
+    public ResponseEntity<String> handleNotFoundConflict(Exception exception) {
+        return ResponseEntity.status(404).body(exception.getMessage());
     }
 }
